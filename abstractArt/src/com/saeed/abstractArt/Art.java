79ea01e8c96af789f1f0e55aeec9db92ff831602
SaeedQatanani <saeedqatananil@gmail.com>
2022-10-29 13:11:53 +0200

    initial commit

---
+++ abstractArt/src/com/saeed/abstractArt/Art.java
@@ -0,0 +1,9 @@
+package com.saeed.abstractArt;
+
+public abstract class Art {
+	private String title;
+	private String author;
+	private String description;
+	public abstract void viewArt();
+	
+}
---
+++ abstractArt/src/com/saeed/abstractArt/Sculpture.java
@@ -0,0 +1,21 @@
+package com.saeed.abstractArt;
+
+public class Sculpture extends Art {
+	private String material;
+	public Sculpture(String material) {
+		super();
+		this.material = material;
+	}
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public void viewArt() {
+		System.out.println("Info about the art work:" + this.material);
+	}
+
+}
